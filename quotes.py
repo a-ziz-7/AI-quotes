16c2d103bfbb63879e2e81f969b0153e2f19245e
a-ziz-7 <aziz.abdusamiev7@gmail.com>
2024-04-02 15:37:51 -0400

    Delete unused images and update text position in quotes.py

--- quotes.py
+++ quotes.py
@@ -70,7 +70,7 @@ def write_text_on_image(image_path, text, output_path):
     font_size = 80
     font = ImageFont.load_default()
     font = font.font_variant(size=font_size)
-    draw.text((100, 1500), text, fill="white", font=font)
+    draw.text((100, 1400), text, fill="white", font=font)
     output_path += "/quote_"+image_path.split("_")[-1]
     image.save(output_path)
 
@@ -81,7 +81,7 @@ def main():
     output_path = "quotes"
     print("Random Image URL:", random_image)
     print("Image saved to:", image_filename)
-    write_text_on_image(image_filename, "12312312321123123123123123123", output_path)
+    write_text_on_image(image_filename, "123123123211231231231\n23123123", output_path)
     
 
 if __name__ == "__main__":
